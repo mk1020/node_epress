d1870b3c3abcf28a6f6dca7d9ae8d193be49daf8
Mikhail Kovalchuk <mmmffjs7438g@gmail.com>
2021-04-17 03:55:42 +0300

    work on post my-profile and get my-avatar

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000
 const usersData = require('./profile/users')
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,36 +23,37 @@ usersData.forEach((user) => (
   })
 ))
 
-usersData.forEach((user) => (
-  app.post(`/my-profile/${user.username}`, (req, res) => {
-    res.send(user)
-  })
-))
+app.post(`/my-profile/:userName`, jsonParser, (req, res) => {
+  const userName = req.params.userName;
+  const userData = req.body;
+
+  if (userData.name && userData.lastName && userData.country) {
+    res.set('Content-Type', 'text/plain');
+    res.send('success')
+  } else {
+    res.status(400).end('Bad request1');
+  }
+})
 
 const dir = './profile/avatars/';
 
-app.get('*', function (req, res) {
-  const isAvatarsDirectory = req.path.includes('my-avatar');  // возможно не самое хорошее решение) не стал заморачиваться)
-  const extCorrect = req.path.includes('.jpg'); //потенциально слабое место) но маловероятно, что в папке будут файлы c расширениями .jpg*
+app.get('/my-avatar/:avatarName', function (req, res) {
+  const fileName = req.params.avatarName;
+  const extCorrect = fileName.includes('.jpg');
 
-  if (isAvatarsDirectory) {
-    if (extCorrect) {
-      const reqPath = req.path.split(path.sep);
-      const fileName = reqPath[reqPath.length - 1];
+  if (extCorrect) {
+    const readStream = fs.createReadStream(dir + fileName);
+    readStream.on('open', () => {
+      res.set('Content-Type', 'image/jpg');
+      readStream.pipe(res)
+    })
 
-      const readStream = fs.createReadStream(dir + fileName);
-      readStream.on('open', () => {
-        res.set('Content-Type', 'image/jpg'); //а зачем нужно задавать этот Content-Type? работает и без него)
-        readStream.pipe(res)
-      })
-
-      readStream.on('error', () => {
-        res.set('Content-Type', 'text/plain');
-        res.status(404).end('Not found');
-      });
-    } else {
-      return res.status(403).end('Forbidden');
-    }
+    readStream.on('error', () => {
+      res.set('Content-Type', 'text/plain');
+      res.status(404).end('Not found');
+    });
+  } else {
+    return res.status(403).end('Forbidden');
   }
 });
 
